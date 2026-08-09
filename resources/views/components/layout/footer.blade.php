f29de65c5ed0b868068e19f29f1a08d1d590a3cc
Ali Hegazy <alihegazy19@outlook.com>
2026-08-09 15:54:12 +0300

    Put the baselayout of the website

---
+++ resources/views/components/layout/footer.blade.php
@@ -0,0 +1,50 @@
+<footer class="custom-footer">
+    <div class="container">
+        <div class="row g-4">
+
+        <div class="col-12 col-md-3">
+            <h5>Logo Or Title</h5>
+            <p class="text-muted-custom mt-3">High quality products for your daily lifestyle.</p>
+        </div>
+
+        <div class="col-12 col-md-3">
+            <h5>Support</h5>
+            <ul class="text-muted-custom">
+            <li class="mb-2">dummy data,this is a dummy data.</li>
+            <li class="mb-2">example@mail.com</li>
+            <li>+000-1111-222</li>
+            </ul>
+        </div>
+
+        <div class="col-12 col-md-3">
+            <h5>Account</h5>
+            <ul>
+            <li><a href="#">My Account</a></li>
+            <li><a href="#">Login / Register</a></li>
+            <li><a href="#">Cart</a></li>
+            <li><a href="#">Wishlist</a></li>
+            <li><a href="#">Shop</a></li>
+            </ul>
+        </div>
+
+        <div class="col-12 col-md-3">
+            <h5>Quick Link</h5>
+            <ul>
+            <li class="mb-4"><a href="#">Contact</a></li>
+            </ul>
+
+            <div class="d-flex gap-3 mt-3">
+            <a href="#" class="social-icon-btn"><i class="bi bi-facebook"></i></a>
+            <a href="#" class="social-icon-btn"><i class="bi bi-twitter-x"></i></a>
+            <a href="#" class="social-icon-btn"><i class="bi bi-instagram"></i></a>
+            <a href="#" class="social-icon-btn"><i class="bi bi-linkedin"></i></a>
+            </div>
+        </div>
+
+        </div>
+
+        <div class="footer-bottom">
+        <p class="mb-0">&copy; Copyright 2026. All right reserved</p>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/components/layout/nav.blade.php
@@ -0,0 +1,44 @@
+<nav class="navbar navbar-expand-lg custom-navbar">
+    <div class="container">
+
+        <a class="navbar-brand fw-bold" href="#">Logo Or Title</a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarContent">
+
+        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+            <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li>
+            <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
+            @guest
+                <li class="nav-item"><a class="nav-link" href="/login">Login</a></li>
+                <li class="nav-item"><a class="nav-link" href="/register">Register</a></li>
+            @endguest
+        </ul>
+
+        <div class="d-flex align-items-center gap-3">
+            <form class="d-flex" role="search">
+            <div class="input-group">
+                <input class="form-control custom-search-input rounded-start-2 py-1 px-3" type="search" placeholder="What are you looking for?">
+                <button class="btn custom-search-btn rounded-end-2 px-3" type="submit">
+                <i class="bi bi-search"></i>
+                </button>
+            </div>
+            </form>
+
+            <a href="#" class="navbar-icon-btn ms-2 position-relative">
+            <i class="bi bi-heart fs-5"></i>
+            <span class="position-absolute top-0 start-100 translate-middle badge rounded-circle badge-wishlist">{{-- Put the number here --}}</span>
+            </a>
+
+            <a href="#" class="navbar-icon-btn ms-2"><i class="bi bi-cart3 fs-5"></i></a>
+            <a href="#" class="navbar-icon-btn ms-2"><i class="bi bi-person fs-5"></i></a>
+        </div>
+
+        </div>
+
+    </div>
+</nav>
